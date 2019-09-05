3eca3abdd39f0d122b375446fe8c6068efc6c2a8
Ela Kowalska <kowalska.el@gmail.com>
2019-09-05 15:00:43 +0200

    play with animations

--- Freezer/index.js
+++ Freezer/index.js
@@ -1,26 +1,36 @@
-const canvas = document.getElementById('canvas');
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+function init() {
+  window.requestAnimationFrame(draw);
+}
+
+function draw() {
+  const canvas = document.getElementById('canvas');
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  ctx = canvas.getContext('2d');
 
 // gets reference to HTML <canvas>
 // <canvas> creates surface for more then one rendering ctx
 
-const ctx = canvas.getContext('2d'); // gets element context, rendering context, to draw actually on canvas
-
-
 // actual drawing is done using the CanvasRenderingContext2D
-ctx.clearRect(0, 0, 800, 600); // clearing ctx
+  ctx.clearRect(0, 0, canvas.width, canvas.height); // clearing ctx
+  ctx.save();
 
-ctx.fillStyle = 'green';
-ctx.fillRect(100, 100, 150, 100);
+  ctx.translate(400, 400);
 
-function draw() {
-  for (let i = 0; i < 6; i++) {
-    for (let j = 0; j < 6; j++) {
-      ctx.fillStyle = 'rgb(' + Math.floor(255 - 42.5 * i) + ',' + Math.floor(255 - 42.5 * j) + ', 0)';
-      ctx.fillRect(j * 25, i * 25, 25, 25);
-    }
+  let time = new Date();
+
+  ctx.rotate(((2 * Math.PI) / 6) * time.getSeconds() + ((2 * Math.PI) / 6000) * time.getMilliseconds());
+  ctx.translate(10, 0);
+
+  for ( i = 0; i < 10000; i++) {
+    ctx.fillStyle = 'white';
+    ctx.fillRect(Math.random() * canvas.width, Math.random() * canvas.height, 1, 1);
   }
+
+  ctx.restore();
+  window.requestAnimationFrame(draw);
 }
 
-draw();
+init();
+
+
