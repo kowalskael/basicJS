32330551c50a6739e87119a8c06dfd60c19d9c49
Ela Kowalska <kowalska.el@gmail.com>
2019-09-05 20:28:20 +0200

    no message

---
+++ Freezer_0/index.js
@@ -0,0 +1,43 @@
+const canvas = document.getElementById('canvas');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+ctx = canvas.getContext('2d');
+
+let point = {
+  x: Math.random() * canvas.width,
+  y: Math.random() * canvas.height,
+  vx: 5,
+  vy: 2,
+  width: 10,
+  height: 10,
+  color: 'white',
+  draw: function() {
+    ctx.fillStyle = this.color;
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
+}
+
+
+function draw() {
+
+  ctx.clearRect(0, 0, canvas.width, canvas.height); // clearing ctx
+  ctx.save();
+
+  point.draw();
+  point.x += point.vx;
+
+  if (point.x + point.width > canvas.width || point.x + point.width < 0) {
+    point.vx = 0;
+  }
+
+  ctx.restore();
+  window.requestAnimationFrame(draw);
+
+}
+
+draw();
+
+
+
+
--- stardust/index.js
+++ stardust/index.js
@@ -14,13 +14,8 @@ function draw() {
 // actual drawing is done using the CanvasRenderingContext2D
   ctx.clearRect(0, 0, canvas.width, canvas.height); // clearing ctx
   ctx.save();
-  ctx.translate(400, 400);
 
   for ( i = 0; i < 50000; i++) {
-    let time = new Date();
-
-    ctx.rotate(((2 * Math.PI) / 6) * time.getSeconds() + ((2 * Math.PI) / 6000) * time.getMilliseconds());
-    ctx.translate(10, 0);
     ctx.fillStyle = 'white';
     ctx.fillRect(Math.random() * canvas.width, Math.random() * canvas.height, 1, 1);
   }
@@ -30,5 +25,3 @@ function draw() {
 }
 
 init();
-
-
