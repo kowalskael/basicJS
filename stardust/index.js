9162fa421771856330a942967845dc56e9b42148
zgapiane <kowalska.el@gmail.com>
2019-09-09 20:54:19 +0200

    loopuje !

--- stardust/index.js
+++ stardust/index.js
@@ -1,7 +1,8 @@
+let isMoving;
+
 function init() {
   window.requestAnimationFrame(draw);
 }
-
 
 const canvas = document.getElementById('canvas');
 canvas.width = window.innerWidth;
@@ -18,8 +19,13 @@ class Point {
   }
 
   move() {
-    this.x = canvas.width - this.x + Math.random() ;
-    this.y = canvas.height - this.y + Math.random();
+    if (isMoving) {
+      this.x = canvas.width - this.x + Math.random() ;
+      this.y = canvas.height - this.y + Math.random();
+    } else {
+      this.x = canvas.width - this.x + Math.random() * 10;
+      this.y = canvas.height - this.y + Math.random() * 10;
+    }
   }
 
   draw() {
@@ -34,6 +40,12 @@ for (let i = 0; i < 1000; i++) {
   points[i] = new Point();
 }
 
+setInterval( () => {
+  setTimeout(() => { isMoving = false }, 1000);
+  setTimeout(() => { isMoving = true }, 2000);
+}, 2000);
+
+
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height); // clearing ctx
 
@@ -45,7 +57,7 @@ function draw() {
   window.requestAnimationFrame(draw);
 }
 
-init();
+init()
 
 
 
