1660a617cf77302d120cf907596a0bae07e73396
Ela Kowalska <kowalska.el@gmail.com>
2019-10-03 17:08:09 +0200

    snake moving, code long and longer

--- Snake/index.js
+++ Snake/index.js
@@ -13,16 +13,35 @@ for (let i = 0; i < canvas.width/scale; i++) {
 }
 
 for (let j = 0; j < 3; j++) {
-	let square = { x: 1, y: 1 + (j + (scale * j)), color: 'black'};
+	let square = { x: 1 + (canvas.width - scale)/2, y: (canvas.width - scale)/2 + (j + (scale * j)), color: 'black'};
 	snake.push(square);
 
-	snake[j].x += (canvas.width - scale)/2;
-	snake[j].y += (canvas.width - scale)/2;
 	snake[0].color = 'green';
-}
 
-for (let j = 0; j < 3; j++) { // funkcja która wylicza mi pozyję węża
 	ctx.fillStyle = snake[j].color;
 	ctx.fillRect(snake[j].x, snake[j].y, scale, scale);
 }
+
+function move() {
+	ctx.clearRect(0, 0, canvas.width, canvas.height); // clearing ctx
+	for (let i = 0; i < canvas.width/scale; i++) {
+		for (let j = 0; j < canvas.height/scale; j++) {
+			ctx.fillStyle = '#e8daba';
+			ctx.fillRect(1 + (i + (scale * i)), 1 + (j + (j * scale)), scale, scale);
+		}
+	}
+
+	for (let j = 0; j < 3; j++) {
+		snake[j].y = snake[j].y - (1 * (scale + 1));
+		ctx.fillStyle = snake[j].color;
+		ctx.fillRect(snake[j].x, snake[j].y, scale, scale);
+
+		if (snake[j].y - scale < 0) {
+			snake[j].y = canvas.height;
+		}
+	};
+
+}
+
+window.setInterval(move, 1000);
 
