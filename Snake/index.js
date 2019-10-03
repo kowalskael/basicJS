7a345ad0510da069c8767f806d37e5b4ec4984a5
zgapiane <kowalska.el@gmail.com>
2019-10-03 09:49:05 +0200

    snake nic specialnego

--- Snake/index.js
+++ Snake/index.js
@@ -2,22 +2,27 @@ const canvas = document.getElementById('canvas');
 ctx = canvas.getContext('2d');
 
 let scale = 10;
-canvas.width = canvas.height = 21 * scale;
-
 let snake = [];
+canvas.width = canvas.height = 21 * (scale + 1);
 
-// plane ma wypełniać całość planszy
-// fruit ma być elementem planszy, który się odznacza ? zmienia status ?
-// snake również ma byc elementem planszy
-
-function draw() {
-	for (let i = 0; i < canvas.width/scale; i++) {
-		for (let j = 0; j < canvas.height/scale; j++) {
-			ctx.fillStyle = 'white';
-			ctx.fillRect(1 + (i + (scale * i)), 1 + (j + (j * scale)), scale, scale);
-		}
+for (let i = 0; i < canvas.width/scale; i++) {
+	for (let j = 0; j < canvas.height/scale; j++) {
+		ctx.fillStyle = '#e8daba';
+		ctx.fillRect(1 + (i + (scale * i)), 1 + (j + (j * scale)), scale, scale);
 	}
 }
 
-setInterval(draw, 3000);
+for (let j = 0; j < 3; j++) {
+	let square = { x: 1, y: 1 + (j + (scale * j)), color: 'black'};
+	snake.push(square);
+
+	snake[j].x += (canvas.width - scale)/2;
+	snake[j].y += (canvas.width - scale)/2;
+	snake[0].color = 'green';
+}
+
+for (let j = 0; j < 3; j++) { // funkcja która wylicza mi pozyję węża
+	ctx.fillStyle = snake[j].color;
+	ctx.fillRect(snake[j].x, snake[j].y, scale, scale);
+}
 
