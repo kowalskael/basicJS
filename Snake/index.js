532bf6bc78e56e884815c24ee416a15ad8664eca
zgapiane <kowalska.el@gmail.com>
2019-10-05 16:17:34 +0200

    snake long version modification

--- Snake/index.js
+++ Snake/index.js
@@ -8,12 +8,12 @@ canvas.width = canvas.height = 21 * (scale + 1);
 for (let i = 0; i < canvas.width/scale; i++) {
 	for (let j = 0; j < canvas.height/scale; j++) {
 		ctx.fillStyle = '#e8daba';
-		ctx.fillRect(1 + (i + (scale * i)), 1 + (j + (j * scale)), scale, scale);
+		ctx.fillRect(1 + i + scale * i, 1 + j + j * scale, scale, scale);
 	}
 }
 
 for (let j = 0; j < 3; j++) {
-	let square = { x: 1 + (canvas.width - scale)/2, y: (canvas.width - scale)/2 + (j + (scale * j)), color: 'black'};
+	let square = { x: 1 + (canvas.width - scale)/2, y: (canvas.width - scale)/2 + j + scale * j, color: 'black'};
 	snake.push(square);
 
 	snake[0].color = 'green';
@@ -27,21 +27,21 @@ function move() {
 	for (let i = 0; i < canvas.width/scale; i++) {
 		for (let j = 0; j < canvas.height/scale; j++) {
 			ctx.fillStyle = '#e8daba';
-			ctx.fillRect(1 + (i + (scale * i)), 1 + (j + (j * scale)), scale, scale);
+			ctx.fillRect(1 + i + scale * i, 1 + j + j * scale, scale, scale);
 		}
 	}
 
 	for (let j = 0; j < 3; j++) {
-		snake[j].y = snake[j].y - (1 * (scale + 1));
+		snake[j].y = snake[j].y - (scale + 1);
 		ctx.fillStyle = snake[j].color;
 		ctx.fillRect(snake[j].x, snake[j].y, scale, scale);
 
 		if (snake[j].y - scale < 0) {
 			snake[j].y = canvas.height;
 		}
-	};
+	}
 
 }
 
-window.setInterval(move, 1000);
+setInterval(move, 1000);
 
