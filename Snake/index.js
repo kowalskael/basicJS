34a3755afba69fa404eaf9756777b0be19180bea
zgapiane <kowalska.el@gmail.com>
2019-10-05 21:03:39 +0200

    snake upgrade, movement, walking through walls, no keys yet

--- Snake/index.js
+++ Snake/index.js
@@ -2,46 +2,47 @@ const canvas = document.getElementById('canvas');
 ctx = canvas.getContext('2d');
 
 let scale = 10;
-let snake = [];
-canvas.width = canvas.height = 21 * (scale + 1);
+canvas.width = canvas.height = 21;
 
-for (let i = 0; i < canvas.width/scale; i++) {
-	for (let j = 0; j < canvas.height/scale; j++) {
-		ctx.fillStyle = '#e8daba';
-		ctx.fillRect(1 + i + scale * i, 1 + j + j * scale, scale, scale);
-	}
-}
+// directions
+let left = { x: -1, y: 0 }; let right = { x: 1, y: 0 }; let up = { x: 0, y: -1 }; let down = { x: 0, y: 1 };
+
+// snake draw
+let snake = [];
 
 for (let j = 0; j < 3; j++) {
-	let square = { x: 1 + (canvas.width - scale)/2, y: (canvas.width - scale)/2 + j + scale * j, color: 'black'};
+	let square = { x: canvas.width/2, y: canvas.width/2 + j};
 	snake.push(square);
 
-	snake[0].color = 'green';
-
-	ctx.fillStyle = snake[j].color;
-	ctx.fillRect(snake[j].x, snake[j].y, scale, scale);
+	snake[0].color = 'aquamarine'; // head is different from body
+	ctx.fillStyle = 'white';
+	ctx.fillRect(snake[j].x, snake[j].y, 1, 1);
 }
 
+console.log(snake);
+
+// snake move
 function move() {
 	ctx.clearRect(0, 0, canvas.width, canvas.height); // clearing ctx
-	for (let i = 0; i < canvas.width/scale; i++) {
-		for (let j = 0; j < canvas.height/scale; j++) {
-			ctx.fillStyle = '#e8daba';
-			ctx.fillRect(1 + i + scale * i, 1 + j + j * scale, scale, scale);
-		}
-	}
+
+	snake.pop();
+
+	let new0 = { x: snake[0].x + up.x, y: snake[0].y + up.y }; // here is place to give variable with chosen keys
+	snake.unshift(new0);
 
 	for (let j = 0; j < 3; j++) {
-		snake[j].y = snake[j].y - (scale + 1);
-		ctx.fillStyle = snake[j].color;
-		ctx.fillRect(snake[j].x, snake[j].y, scale, scale);
+		if (snake[j].x - 1 < 0) {
+			snake[j].x = canvas.width;
+		}
 
-		if (snake[j].y - scale < 0) {
+		if (snake[j].y - 1 < 0) {
 			snake[j].y = canvas.height;
 		}
-	}
 
+		ctx.fillRect(snake[j].x, snake[j].y, 1, 1);
+	}
 }
+
 
 setInterval(move, 1000);
 
