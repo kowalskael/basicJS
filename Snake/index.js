1d476834f3ea165731dbaec0a5a0ab753de463cf
Ela Kowalska <kowalska.el@gmail.com>
2019-10-09 15:53:11 +0200

    snake corrections vol. 1

--- Snake/index.js
+++ Snake/index.js
@@ -2,10 +2,10 @@ const canvas = document.getElementById('canvas');
 ctx = canvas.getContext('2d');
 
 let scale = 10;
-canvas.width = canvas.height = 21 * (scale + 1);
+canvas.width = canvas.height = 21 * scale;
 let fruit = { x: 1, y: 1 };
 let snake = [];
-
+let intervalID;
 let direction = { x: 0, y: -1 };
 
 function control(e) {
@@ -29,67 +29,68 @@ addEventListener( "keydown", control);
 
 // snake
 for (let j = 0; j < 3; j++) {
-	let square = { x: 1 + 11 * scale, y: 1 + 11 * scale + (j + (scale * j)), color: '#0cf2b9'};
+	let square = { x: 11, y: 11 + j, color: 'black'};
 	snake.push(square);
 }
 
 // snake move
 function move() {
 
-	ctx.clearRect(0, 0, 21 * (scale + 1), 21 * (scale + 1)); // clearing ctx
+	ctx.clearRect(0, 0, canvas.width, canvas.height ); // clearing ctx
 
-	let new0 = {x: snake[0].x + (direction.x * (scale + 1)), y: snake[0].y + (direction.y * (scale + 1)), color: '#0cf2b9'}; // here is place to give variable with chosen key
+	let new0 = {x: snake[0].x + direction.x, y: snake[0].y + direction.y, color: '#0cf2b9'}; // here is place to give variable with chosen key
 	snake.unshift(new0);
 
 	// hit detection
 	if (snake[0].x === fruit.x && snake[0].y === fruit.y) {
 		for (let j = 0; j < snake.length; j++) {
 			do {
-				fruit.x = 1 + (Math.floor(Math.random() * 21)) * (scale + 1);
-				fruit.y = 1 + (Math.floor(Math.random() * 21)) * (scale + 1);
-			} while (fruit.x === snake[j].x && fruit.y === snake[j].y);
+				fruit.x = (Math.floor(Math.random() * 21));
+				fruit.y = (Math.floor(Math.random() * 21));
+			} while (fruit.x ==! snake[j].x && fruit.y ==! snake[j].y);
 		}
 	} else {
 		snake.pop();
 	}
 
-	for (let j = 0; j < snake.length; j++) {
+	for (let j = 1; j < snake.length; j++) {
 		if (snake[j].x < 0) {
-			snake[j].x = 1 + 20 * (scale + 1);
+			snake[j].x = 20;
 		}
-		if (snake[j].x > canvas.width) {
-			snake[j].x = 1;
+		if (snake[j].x > 20) {
+			snake[j].x = 0;
 		}
 		if (snake[j].y < 0) {
-			snake[j].y = 1 + 20 * (scale + 1);
+			snake[j].y = 20;
 		}
-		if (snake[j].y > canvas.height) {
-			snake[j].y = 1;
+		if (snake[j].y > 20) {
+			snake[j].y = 0;
 		}
 		if (snake[0].x === snake[j].x && snake[0].y === snake[j].y) {
-			// game over
+			clearInterval(intervalID);
 		}
-	}
 
-	for (let i = 0; i < canvas.width/scale; i++) {
-		for (let j = 0; j < canvas.height/scale; j++) {
-			ctx.fillStyle = '#f5eeda';
-			ctx.fillRect(1 + i + scale * i, 1 + (j + (j * scale)), scale, scale);
+		for (let j = 0; j < snake.length; j++) {
+			snake[0].color = 'black';
+			snake[1].color = 'black';
+			ctx.fillStyle = snake[j].color;
+			ctx.fillRect(snake[j].x * scale, snake[j].y * scale, scale, scale);
 		}
-	}
 
-	for (let j = 0; j < snake.length; j++) {
-		snake[0].color = 'black';
-		snake[1].color = '#0cf2b9';
-		ctx.fillStyle = snake[j].color;
-		ctx.fillRect(snake[j].x, snake[j].y, scale, scale);
+		ctx.fillStyle = '#EA244A';
+		ctx.fillRect(fruit.x * scale, fruit.y * scale, scale, scale);
 	}
-
-	ctx.fillStyle = '#ffbf0a';
-	ctx.fillRect(fruit.x, fruit.y, scale, scale);
 }
 
-setInterval(move, 1000);
+function draw() {
+	intervalID = setInterval(move, 1000);
+}
+
+draw();
+
+
+
+
 
 
 
