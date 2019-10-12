1b5e73744509a1991a432a7aaa7a4db509ce9df8
zgapiane <kowalska.el@gmail.com>
2019-10-12 19:19:37 +0200

    snake, clean up && shortening

--- Snake/index.js
+++ Snake/index.js
@@ -4,12 +4,11 @@ ctx = canvas.getContext('2d');
 let scale = 10;
 canvas.width = canvas.height = 21 * scale;
 
-let fruit = { x: 0, y: 1 };
+let fruit = { x: 1, y: 1 };
 
 let snake = [];
 for (let j = 0; j < 3; j++) {
-	let square = { x: 11, y: 11 + j, color: 'black'};
-	snake.push(square);
+	snake.push( { x: 11, y: 11 + j } );
 }
 
 let intervalMove;
@@ -20,30 +19,19 @@ function move() {
 
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-	let newHead = {x: snake[0].x + direction.x, y: snake[0].y + direction.y};
-	snake.unshift(newHead);
+	snake.unshift({x: snake[0].x + direction.x, y: snake[0].y + direction.y});
 
 	// walls detection
 	for (let j = 0; j < snake.length; j++) {
-		if (snake[j].x < 0) {
-			snake[j].x = 20;
-		}
-		if (snake[j].x > 20) {
-			snake[j].x = 0;
-		}
-		if (snake[j].y < 0) {
-			snake[j].y = 20;
-		}
-		if (snake[j].y > 20) {
-			snake[j].y = 0;
-		}
+		if (snake[j].x < 0) snake[j].x = 20;
+		if (snake[j].x > 20) snake[j].x = 0;
+		if (snake[j].y < 0) snake[j].y = 20;
+		if (snake[j].y > 20) snake[j].y = 0;
 	}
 
 	// fruit detection && position randomization
 	if (snake[0].x === fruit.x && snake[0].y === fruit.y) {
-		do {
-			fruit.x = Math.floor(Math.random() * 21);
-			fruit.y = Math.floor(Math.random() * 21);
+		do { fruit.x = fruit.y = Math.floor(Math.random() * 21);
 		} while (snake.find(obj => obj.x === fruit.x && obj.y === fruit.y ));
 	} else {
 		snake.pop();
@@ -56,16 +44,13 @@ function move() {
 		}
 	}
 
-	snake[0].color = 'white';
-	ctx.fillStyle = snake[0].color;
-	ctx.fillRect(snake[0].x * scale, snake[0].y * scale, scale, scale);
-
-	for (let j = 1; j < snake.length; j++) {
-		snake[1].color = 'black';
-		ctx.fillStyle = snake[j].color;
+	// snake draw
+	for (let j = 0; j < snake.length; j++) {
+		ctx.fillStyle = j === 0 ? 'white' : 'black';
 		ctx.fillRect(snake[j].x * scale, snake[j].y * scale, scale, scale);
 	}
 
+	// fruit draw
 	ctx.fillStyle = '#EA244A';
 	ctx.fillRect(fruit.x * scale, fruit.y * scale, scale, scale);
 }
@@ -77,24 +62,16 @@ intervalMove = setInterval(move, 500);
 addEventListener( "keydown", function(e) {
 	switch(e.key) {
 		case 'ArrowUp':
-			if( direction.y !== 1) {
-				direction = { x: 0, y: -1 };
-			}
+			if( direction.y !== 1) { direction = { x: 0, y: -1 }; }
 			break;
 		case 'ArrowRight':
-			if( direction.x !== -1) {
-				direction = {x: 1, y: 0};
-			}
+			if( direction.x !== -1) { direction = {x: 1, y: 0 }; }
 			break;
 		case 'ArrowLeft':
-			if( direction.x !== 1) {
-				direction = {x: -1, y: 0};
-			}
+			if( direction.x !== 1) { direction = {x: -1, y: 0 }; }
 			break;
 		case 'ArrowDown':
-			if(direction.y !== -1) {
-				direction = { x: 0, y: 1 };
-			}
+			if(direction.y !== -1) { direction = { x: 0, y: 1 }; }
 			break;
 	}
 });
