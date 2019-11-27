15c159104de5024e6753dd5f17514155a40e111c
zgapiane <kowalska.el@gmail.com>
2019-11-27 19:13:22 +0100

    Saper, basic html, css

--- Saper/index.js
+++ Saper/index.js
@@ -1,24 +1,60 @@
-let area = [];
+document.getElementById("play").style.display = "none";
 
-let rows = 10;
-let columns = 10;
+document.getElementById("submit").onclick = function() {
+	document.getElementById("start").style.display = "none"; // saper menu hidden
+	document.getElementById("play").style.display = "block"; // show play area
 
-for (let i = 0; i < rows; i++) {
-		area[i] = [];
-}
+	//timer
+	let time = document.getElementById("timer"); // refer to timer element
 
-for (let i = 0; i < rows; i++) {
-	for (let j = 0; j < columns; j++) {
-		area[i][j] = { fill: 0, state: 'hidden' };
+	let seconds = 0; let minutes = 0;
+
+	function add() { // update numbers in timer
+		seconds++;
+		if(seconds >= 60) {
+			minutes++;
+			seconds = 0;
+		}
+
+		time.textContent = (minutes ? (minutes > 9 ? minutes : '0' + minutes) : '00') + ':' + (seconds > 9 ? seconds : '0' + seconds);
+
+		timer();
 	}
-}
 
-let seconds = 0;
-let minutes = 0;
+	function timer() {
+	 setTimeout(add, 1000);
+	}
 
-setInterval(function() {
-	document.getElementById("timer").innerHTML = minutes + ' : ' + seconds++;
-}, 1000);
+	timer();
 
-console.log(area);
+	// play area
+	let rows = document.getElementById("width").value;
+	let columns = document.getElementById("height").value;
+	let bombNums = document.getElementById("bombNums").value;
+
+	let areas = [];
+
+	const square = { fill: 0, state: 'hidden' };
+
+	let board = document.getElementById("board");
+
+	for (let i = 0; i < rows; i++) {
+		areas[i] = [];
+		let row = document.createElement('div');
+		board.appendChild(row);
+		row.setAttribute("id", "row");
+	}
+
+	for (let i = 0; i < rows; i++) {
+		for (let j = 0; j < columns; j++) {
+			areas[i][j] = { fill: 0, state: 'hidden' };
+		}
+	}
+
+
+	console.log(board);
+	console.log(areas);
+};
+
+
 
