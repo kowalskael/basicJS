17dc7e9d51b91a831e2d15eebddd9b04c9b014d5
zgapiane <kowalska.el@gmail.com>
2019-11-29 16:34:52 +0100

    Saper, objArr, divArr

--- Saper/index.js
+++ Saper/index.js
@@ -1,6 +1,7 @@
 document.getElementById("play").style.display = "none";
 
 document.getElementById("submit").onclick = function() {
+
 	document.getElementById("start").style.display = "none"; // saper menu hidden
 	document.getElementById("play").style.display = "block"; // show play area
 
@@ -16,13 +17,14 @@ document.getElementById("submit").onclick = function() {
 			seconds = 0;
 		}
 
-		time.textContent = (minutes ? (minutes > 9 ? minutes : '0' + minutes) : '00') + ':' + (seconds > 9 ? seconds : '0' + seconds);
+		time.textContent = (minutes ? (minutes > 9 ? minutes : '0' + minutes) : '00')
+			+ ':' + (seconds > 9 ? seconds : '0' + seconds);
 
 		timer();
 	}
 
 	function timer() {
-	 setTimeout(add, 1000);
+	 setTimeout(add, 1000); // call function every 1s
 	}
 
 	timer();
@@ -32,28 +34,35 @@ document.getElementById("submit").onclick = function() {
 	let columns = document.getElementById("height").value;
 	let bombNums = document.getElementById("bombNums").value;
 
-	let areas = [];
+	let objArr = []; // two-dimensional array
+	const square = { fill: 0, state: 'hidden' }; // object with parameters about Saper squares
 
-	const square = { fill: 0, state: 'hidden' };
-
-	let board = document.getElementById("board");
-
-	for (let i = 0; i < rows; i++) {
-		areas[i] = [];
-		let row = document.createElement('div');
-		board.appendChild(row);
-		row.setAttribute("id", "row");
-	}
-
-	for (let i = 0; i < rows; i++) {
+	for (let i = 0; i < rows; i++) { // create array with objects
+		objArr[i] = [];
 		for (let j = 0; j < columns; j++) {
-			areas[i][j] = { fill: 0, state: 'hidden' };
+			objArr[i][j] = square;
 		}
 	}
 
+	//create Array with div's
+	let divContainer = document.getElementById("board"); // variable returning html element
+	let divArr = [];
 
-	console.log(board);
-	console.log(areas);
+	for (let i = 0; i < rows; i++) { // create array with div's
+		divArr[i] = [];
+		let row = document.createElement('div');
+		divContainer.appendChild(row);
+		for (let j = 0; j < columns; j++) {
+			let column = document.createElement('div');
+			row.appendChild(column);
+			column.setAttribute("class", 'row' + i + 'column' + j);
+			divArr[i][j] = document.getElementsByClassName('row' + i + 'column' + j);
+		}
+	}
+
+	console.log(board.childNodes);
+	console.log(objArr);
+	console.log(divArr);
 };
 
 
