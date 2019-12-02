1a3eb5fbe17f36dce02a32fd62df35347620e916
zgapiane <kowalska.el@gmail.com>
2019-12-02 13:25:04 +0100

    Saper, więcej pseudokodu

--- Saper/index.js
+++ Saper/index.js
@@ -15,6 +15,9 @@ document.getElementById("submit").onclick = function() {
 		if(seconds >= 60) {
 			minutes++;
 			seconds = 0;
+			if(minutes >= 60) {
+				// end of game
+			}
 		}
 
 		time.textContent = (minutes ? (minutes > 9 ? minutes : '0' + minutes) : '00')
@@ -36,34 +39,69 @@ document.getElementById("submit").onclick = function() {
 
 	// create Array with objects
 	let objArr = []; // two-dimensional array
-	const square = { fill: 0, state: 'hidden' }; // object with parameters about Saper squares
+	const square = { fill: 0, state: 'hidden', x: 1, y: 1 }; // object with parameters about Saper squares
+
+	let divContainer = document.getElementById("board"); // variable returning html element
 
 	for (let i = 0; i < rows; i++) { // create array with objects
 		objArr[i] = [];
-		for (let j = 0; j < columns; j++) {
-			objArr[i][j] = square;
-		}
-	}
-
-	// create Array with div's
-	let divContainer = document.getElementById("board"); // variable returning html element
-	let divArr = [];
-
-	for (let i = 0; i < rows; i++) { // create array with div's
-		divArr[i] = [];
 		let row = document.createElement('div');
 		divContainer.appendChild(row);
 		for (let j = 0; j < columns; j++) {
+			objArr[i][j] = square;
 			let column = document.createElement('div');
 			row.appendChild(column);
-			column.setAttribute("class", 'hidden'); // every element with class hidden for start
-			divArr[i][j] = document.getElementsByClassName('hidden');
+			column.setAttribute("class", 'hidden square'); // every element with class hidden for start
 		}
 	}
-	
-	console.log(divContainer.childNodes);
-	console.log(objArr);
-	console.log(divArr);
+
+	// drawing bombs, after 1st click
+	function drawBombs() {
+		// losowanie i z dwu-wymiarowej tablicy objArr
+		// losowanie j z dwu-wymiarowej tablicy objArr
+		// bombs[i][j] to wylosowane [i] + [j] z objArr
+		// wylosowanym obiektom zmień fill: 'bomb'
+	}
+
+	// objects with numbers
+	function flagNumbers() {
+		// check objects adjacent to bombs
+		// foundNum = sum of all found bombs around
+		// if (adjacentObjects[i][j]) have fill: 9, foundNum + foundBomb
+		// if(foundNum > 0 && foundNum < 9) object fill: foundNum
+	}
+
+	// flagging squares
+	function flagObject() {
+		// if object with state: 'hidden' is clicked with right button of the mouse
+		// change object state: 'flagged'
+		// if object with state: 'flagged' is clicked with left button of the mouse
+		// change object state: 'hidden'
+
+	}
+
+	// revealing objects
+	function revealObject() {
+		
+	}
+
+	// select all div's in board
+	let squareDivs = document.querySelectorAll('div.square');
+	console.log(squareDivs);
+
+	//  bind object square fill & state to individual div
+
+
+	/*// change class on the every div that is clicked
+	for(let i = 0; i < squareDivs.length; i++) {
+		squareDivs[i].onclick = function() {
+			// change fill or state of object
+		};
+		squareDivs[i].ondblclick = function() {
+			// change fill or state of object
+		}
+	}
+	*/
 };
 
 
