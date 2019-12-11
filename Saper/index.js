62e5e0b9dc06804a98221de0a1925e4f61347964
zgapiane <kowalska.el@gmail.com>
2019-12-11 12:20:46 +0100

    Saper, createBoard()

--- Saper/index.js
+++ Saper/index.js
@@ -1,144 +1,71 @@
 document.getElementById("play").style.display = "none";
-
+let objArr = [];
 document.getElementById("submit").onclick = function() {
 
 	document.getElementById("start").style.display = "none"; // saper menu hidden
 	document.getElementById("play").style.display = "block"; // show play area
 
 	// TIMER
-	let time = document.getElementById("timer"); // refer to timer element
 
-	let seconds = 0; let minutes = 0;
-
-	function add() { // update numbers in timer
-		seconds++;
-		if(seconds >= 60) {
-			minutes++;
-			seconds = 0;
-			if(minutes >= 60) {
-				// end of game
-			}
-		}
-
-		time.textContent = (minutes ? (minutes > 9 ? minutes : '0' + minutes) : '00')
-			+ ':' + (seconds > 9 ? seconds : '0' + seconds);
-
-		timer();
-	}
-
-	function timer() {
-		setTimeout(add, 1000); // call function every 1s
-	}
-
-	timer();
-
-	// PLAY
+	// start
 	let rows = document.getElementById("width").value;
 	let columns = document.getElementById("height").value;
-	let bombNums = document.getElementById("bombNums").value;
+	let numBombs = document.getElementById("bombNums").value;
 
-	// create Array with objects
-	let objArr = []; // two-dimensional array
-	let divContainer = document.getElementById("board"); // variable returning html element
+	function createBoard(width, height, numBombs) {
 
-	for (let row = 0; row < rows; row++) { // create array with objects
-		objArr[row] = [];
-		let rowDiv = document.createElement('div');
-		divContainer.appendChild(rowDiv);
-		for (let col = 0; col < columns; col++) {
-			let columnDiv = document.createElement('div');
-			rowDiv.appendChild(columnDiv);
-			columnDiv.classList.add('hidden'); // every element with class hidden for start
-			columnDiv.classList.add('square');
-			objArr[row][col] = { fill: 0, state: 'hidden', element: columnDiv };
-		}
-	}
-
-	// drawing bombs, after 1st click
-	function drawBombs() { // do drawing for first click, then increase clickCount and 'block' function
-		let clickCount = 0;
-		return function(e) {
-			if (clickCount === 0) {
-				for (let row = 0; row < objArr.length; row++) {
-					for (let col = 0; col < objArr[row].length; col++) {
-						if ( objArr[row][col].element === e.target) {
-							objArr[row][col].state = 'revealed';
-						}
-					}
-				}
-
-				let loopCount = 0;
-
-				do {
-					let row = Math.floor(Math.random() * objArr.length);
-					let col = Math.floor(Math.random() * objArr[row].length);
-					objArr[row][col].fill = 9;
-					loopCount++;
-				} while ( loopCount < bombNums );
+		//create array
+		let board = [];
+		for (let row = 0; row < width; row++) { // create array with objects
+			board[row] = [];
+			for (let col = 0; col < height; col++) {
+				board[row][col] = { fill: 0, state: 'hidden' };
 			}
-
-			clickCount++;
 		}
-	}
 
-	divContainer.onclick = drawBombs();
-
-	// objects with numbers
-	function flagNumbers() {
+		// drawing bombs
+		for (let bomb = 0; bomb <= numBombs; bomb +=1 ) {
+			do {
+				let row = Math.floor(Math.random() * board.length);
+				let col = Math.floor(Math.random() * board[row].length);
+				board[row][col].fill = 9;
+			} while (board.find(index => index.fill === 9));
+		}
 
 		let checkId = [ // array for checking id
-			{x: -1, y: -1}, {x: -1, y: 0}, {x: -1, y: +1},
-			{x: 0, y: -1}, {x: 0, y: +1},
-			{x: +1, y: -1}, {x: +1, y: 0}, {x: +1, y: +1}];
+			{row: -1, col: -1}, {row: -1, col: 0}, {row: -1, col: +1},
+			{row: 0, col: -1}, {row: 0, col: +1},
+			{row: +1, col: -1}, {row: +1, col: 0}, {row: +1, col: +1}];
 
-		for (let row = 0; row < objArr.length; row++) {
-			for (let col = 0; col < objArr[row].length; col++) {
+		// assign numbers
+		for (let row = 0; row < board.length; row++) {
+			for (let col = 0; col < board[row].length; col++) {
 
 				let numberOfNeighbourBombs = 0;
-				if ( objArr[row][col].fill === 9) continue;
+				if ( board[row][col].fill === 9) continue;
 
-					for (let check = 0; check < checkId.length; check++) { // dla każdego elementu z checkId
+				for (let check = 0; check < checkId.length; check += 1) {
 
-						const dir = checkId[check];
-						if (row + dir.x >= 0 && row + dir.x < objArr.length && col + dir.y >= 0 && col + dir.y < objArr[row].length)  // pola do sprawdzenia, występujące w tabicy
-							if ( objArr[row + dir.x][col + dir.y] === 9)
-								numberOfNeighbourBombs += 1;
+					let dir = checkId[check];
+
+					if (row + dir.row >= 0 && row + dir.row < board.length && col + dir.col >= 0 && col + dir.col < board.length) {
+
+						if ( board[row + dir.row][col + dir.col].fill === 9) {
+							numberOfNeighbourBombs += 1;
+						}
 					}
 
-					objArr[row][col].fill = numberOfNeighbourBombs;
+					board[row][col].fill = numberOfNeighbourBombs;
+
+				}
 			}
 		}
+
+		return board;
 	}
 
-	flagNumbers();
-
-	console.log(objArr);
-
-	// flagging squares
-	function flagObject() {
-		// if object with state: 'hidden' is clicked with right button of the mouse
-		// change object state: 'flagged'
-		// if object with state: 'flagged' is clicked with left button of the mouse
-		// change object state: 'hidden'
-
-	}
-
-	// revealing objects
-	function revealObject() {
-		// if object.state: 'hidden' onclick change state: 'revealed'
-		//
-	}
-
-	/*// change class on the every div that is clicked
-	for(let i = 0; i < squareDivs.length; i++) {
-		squareDivs[i].onclick = function() {
-			// change fill or state of object
-		};
-		squareDivs[i].ondblclick = function() {
-			// change fill or state of object
-		}
-	}
-	*/
+	let board = createBoard(rows, columns, numBombs);
+	console.table(board);
 };
 
 
