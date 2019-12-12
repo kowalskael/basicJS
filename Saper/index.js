753f88337c97210add2bf9b8952a10d8325efa4a
zgapiane <kowalska.el@gmail.com>
2019-12-12 09:38:58 +0100

    Saper, userClicks test

--- Saper/index.js
+++ Saper/index.js
@@ -1,5 +1,5 @@
 document.getElementById("play").style.display = "none";
-let objArr = [];
+
 document.getElementById("submit").onclick = function() {
 
 	document.getElementById("start").style.display = "none"; // saper menu hidden
@@ -7,11 +7,17 @@ document.getElementById("submit").onclick = function() {
 
 	// TIMER
 
-	// start
 	let rows = document.getElementById("width").value;
 	let columns = document.getElementById("height").value;
 	let numBombs = document.getElementById("bombNums").value;
 
+	// array for checking neighbours
+	let checkId = [
+		{row: -1, col: -1}, {row: -1, col: 0}, {row: -1, col: +1},
+		{row: 0, col: -1}, {row: 0, col: +1},
+		{row: +1, col: -1}, {row: +1, col: 0}, {row: +1, col: +1}];
+
+	// start
 	function createBoard(width, height, numBombs) {
 
 		//create array
@@ -31,11 +37,6 @@ document.getElementById("submit").onclick = function() {
 				board[row][col].fill = 9;
 			} while (board.find(index => index.fill === 9));
 		}
-
-		let checkId = [ // array for checking id
-			{row: -1, col: -1}, {row: -1, col: 0}, {row: -1, col: +1},
-			{row: 0, col: -1}, {row: 0, col: +1},
-			{row: +1, col: -1}, {row: +1, col: 0}, {row: +1, col: +1}];
 
 		// assign numbers
 		for (let row = 0; row < board.length; row++) {
@@ -64,8 +65,69 @@ document.getElementById("submit").onclick = function() {
 		return board;
 	}
 
-	let board = createBoard(rows, columns, numBombs);
-	console.table(board);
+	//let board = createBoard(rows, columns, numBombs);
+	//console.table(board);
+
+	//console.log(JSON.stringify(board));
+
+	//play
+	function userPlay(board, row, col) {
+
+		if (board[row][col].fill === 0) {
+
+			board[row][col].state = 'revealed';
+
+			for (let check = 0; check < checkId.length; check += 1) {
+
+				let dir = checkId[check];
+
+				if (row + dir.row >= 0 && row + dir.row < board.length && col + dir.col >= 0 && col + dir.col < board.length) {
+
+						if (0 < board[row + dir.row][col + dir.col].fill && board[row + dir.row][col + dir.col].fill <= 8)
+							board[row + dir.row][col + dir.col].state = 'revealed';
+
+					if (board[row + dir.row][col + dir.col].fill === 0)
+						board[row + dir.row][col + dir.col].state = 'revealed';
+
+				}
+			}
+
+				// jeśli obiekt sąsiadujący ma fill: 1-8, return obiekt.state: 'revealed';
+				// jeśli obiekt sąsiadujący ma fill: 0, zmień jego state: 'revealed', wywołaj na nim funkcję clickEmptyOrDoubleClick()
+				// warunek stopujący rekurencję to brak obiektów fill: 0 && state hidden w sąsiedztwie
+
+		}
+
+		if (board[row][col].fill > 0 && board[row][col].fill <= 8) {
+			board[row][col].state = 'revealed';
+		}
+
+		if (board[row][col].fill === 9) {
+			board[row][col].state = 'revealed';
+			// end of game
+		}
+
+		return board;
+	}
+
+	let boardTest = [[{"fill":2,"state":"hidden"},{"fill":2,"state":"hidden"},{"fill":1,"state":"hidden"},{"fill":0,"state":"hidden"},{"fill":0,"state":"hidden"},{"fill":0,"state":"hidden"},{"fill":0,"state":"hidden"},{"fill":0,"state":"hidden"}],[{"fill":9,"state":"hidden"},{"fill":9,"state":"hidden"},{"fill":1,"state":"hidden"},{"fill":0,"state":"hidden"},{"fill":0,"state":"hidden"},{"fill":1,"state":"hidden"},{"fill":1,"state":"hidden"},{"fill":1,"state":"hidden"}],[{"fill":2,"state":"hidden"},{"fill":3,"state":"hidden"},{"fill":2,"state":"hidden"},{"fill":1,"state":"hidden"},{"fill":0,"state":"hidden"},{"fill":1,"state":"hidden"},{"fill":9,"state":"hidden"},{"fill":1,"state":"hidden"}],[{"fill":0,"state":"hidden"},{"fill":1,"state":"hidden"},{"fill":9,"state":"hidden"},{"fill":1,"state":"hidden"},{"fill":1,"state":"hidden"},{"fill":2,"state":"hidden"},{"fill":2,"state":"hidden"},{"fill":1,"state":"hidden"}],[{"fill":0,"state":"hidden"},{"fill":1,"state":"hidden"},{"fill":1,"state":"hidden"},{"fill":1,"state":"hidden"},{"fill":1,"state":"hidden"},{"fill":9,"state":"hidden"},{"fill":1,"state":"hidden"},{"fill":0,"state":"hidden"}],[{"fill":0,"state":"hidden"},{"fill":0,"state":"hidden"},{"fill":1,"state":"hidden"},{"fill":1,"state":"hidden"},{"fill":3,"state":"hidden"},{"fill":2,"state":"hidden"},{"fill":3,"state":"hidden"},{"fill":1,"state":"hidden"}],[{"fill":1,"state":"hidden"},{"fill":1,"state":"hidden"},{"fill":3,"state":"hidden"},{"fill":9,"state":"hidden"},{"fill":3,"state":"hidden"},{"fill":9,"state":"hidden"},{"fill":3,"state":"hidden"},{"fill":9,"state":"hidden"}],[{"fill":1,"state":"hidden"},{"fill":9,"state":"hidden"},{"fill":3,"state":"hidden"},{"fill":9,"state":"hidden"},{"fill":3,"state":"hidden"},{"fill":2,"state":"hidden"},{"fill":9,"state":"hidden"},{"fill":2,"state":"hidden"}]];
+
+	let userClickedOnBoard = userPlay(boardTest, 5, 1);
+	console.table(userClickedOnBoard);
+
+	let boardContainer = document.getElementById('board');
+
+	for (let i = 0; i < boardTest.length; i++) {
+		let rows = document.createElement("div");
+		boardContainer.append(rows);
+		for (let j = 0; j < boardTest[i].length; j++) {
+			let cols = document.createElement("div");
+			rows.append(cols);
+			cols.classList.add(boardTest[i][j].state);
+			boardTest[i][j].element = cols;
+		}
+	}
+
 };
 
 
