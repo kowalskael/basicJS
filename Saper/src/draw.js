0840fb1e482fad7eb4cc1db4223d5771491b389a
Elzbieta Kowalska <kowalska.el@gmail.com>
2019-12-21 19:19:15 +0100

    Saper, draw() out

--- Saper/src/draw.js
+++ Saper/src/draw.js
@@ -2,20 +2,20 @@ import { Board } from './Board';
 
 export function draw(boardContainer) {
 
-    for (let i = 0; i < Board.board.length; i++) {
+    for (let i = 0; i < Board.length; i++) {
         const rows = document.createElement("div");
         boardContainer.append(rows);
-        for (let j = 0; j < Board.board[i].length; j++) {
+        for (let j = 0; j < Board[i].length; j++) {
             const cols = document.createElement("div");
             rows.append(cols);
-            cols.classList.add(Board.board[i][j].state);
-            if (Board.board[i][j].fill > 0 && Board.board[i][j].fill < 9) {
-                if (Board.board[i][j].state === 'revealed' || Board.board[i][j].state === 'number') {
-                    cols.innerHTML = Board.board[i][j].fill;
+            cols.classList.add(Board[i][j].state);
+            if (Board[i][j].fill > 0 && Board[i][j].fill < 9) {
+                if (Board[i][j].state === 'revealed' || Board[i][j].state === 'number') {
+                    cols.innerHTML = Board[i][j].fill;
                 }
             }
-            if (Board.board[i][j].fill === 9) {
-                if (Board.board[i][j].state === 'bomb') {
+            if (Board[i][j].fill === 9) {
+                if (Board[i][j].state === 'bomb') {
                     cols.innerHTML = 'B';
                 }
 
